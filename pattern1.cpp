06cb51448e0959f579e80407fe7e07dd94784160
sanyaverma2001 <sanyaverma99789@gmail.com>
2022-04-27 20:18:05 +0530

    pattern1 code

--- pattern1.cpp
+++ pattern1.cpp
@@ -2,24 +2,24 @@
 using namespace std;
 int main()
 {
-    int a = 1;
+    int x = 1;
     int n;
     cin >> n;
     for (int i = 0; i < n; i++)
     {
         cout << !(i % 2);
-        a = !(i % 2);
+        x = !(i % 2);
         for (int j = 0; j < i; j++)
         {
-            if (a == 0)
+            if (x == 0)
             {
                 cout << 1 << " ";
-                a = 1;
+                x = 1;
             }
             else
             {
                 cout << 0 << "  ";
-                a = 0;
+                x = 0;
             }
         }
         cout << "\n";
